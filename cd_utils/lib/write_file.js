a7105a7fbca00ce4f4be39148ec724df67787b77
calenicolas <>
2023-09-01 18:14:14 -0400

    Fix al guardar instancia

--- cd_utils/lib/write_file.js
+++ cd_utils/lib/write_file.js
@@ -3,5 +3,5 @@ const fs = require('fs');
 module.exports = readFile;
 
 function readFile(fileName, content) {
-    fs.writeFileSync(fileName, content);
+    fs.writeFileSync(fileName, JSON.stringify(content));
 }
