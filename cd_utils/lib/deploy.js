9205732066d8bda6b5ae85e22330e58201b3afc5
calenicolas <>
2023-09-09 19:48:50 -0400

    Fix de parametro containerName

--- cd_utils/lib/deploy.js
+++ cd_utils/lib/deploy.js
@@ -7,14 +7,14 @@ const instancesFileName = "/root/deploy-service/deploy/instances.json";
 
 function getParameters(jsonData) {
     const imageName = jsonData.image_name;
-    const appName = jsonData.app_name;
+    const containerName = jsonData.app_name;
     const servicePort = jsonData.service_port;
-    const containerNetwork = appName.substring(0, 15);
+    const containerNetwork = containerName.substring(0, 15);
     const proxyContainerName = jsonData.proxy_container_name;
     const serverName = jsonData.server_name;
 
     return {
-        imageName, appName, servicePort, containerNetwork, proxyContainerName, serverName
+        imageName, containerName, servicePort, containerNetwork, proxyContainerName, serverName
     };
 }
 
