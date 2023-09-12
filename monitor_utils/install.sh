1b7f8d2d5d0cdef18a324dcde14bf74752f1ed1e
calenicolas <>
2023-09-12 12:40:00 -0400

    Monitor utils: fix en el install

--- monitor_utils/install.sh
+++ monitor_utils/install.sh
@@ -4,8 +4,8 @@ mkdir -p /usr/local/lib/monitor_utils || true
 cp -r lib/* /usr/local/lib/monitor_utils
 npm install yaml -C /usr/local/lib/monitor_utils
 
-local PROMETHEUS_CONFIG_PATH="/etc/adlamin/monitor"
-local PROMETHEUS_CONFIG="$PROMETHEUS_CONFIG_PATH/prometheus.yml"
+PROMETHEUS_CONFIG_PATH="/etc/adlamin/monitor"
+PROMETHEUS_CONFIG="$PROMETHEUS_CONFIG_PATH/prometheus.yml"
 
 mkdir -p $PROMETHEUS_CONFIG_PATH/data || true
 cp prometheus.yml $PROMETHEUS_CONFIG
