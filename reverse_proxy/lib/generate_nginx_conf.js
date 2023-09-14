b319b9ca05a66d15c1d90ac15236f9a74879aa2d
calenicolas <>
2023-09-13 23:49:57 -0400

    Reverse Proxy: se permite archivo con clients para generar la configuracion de nginx

--- reverse_proxy/lib/generate_nginx_conf.js
+++ reverse_proxy/lib/generate_nginx_conf.js
@@ -8,15 +8,25 @@ const serverName = args[1];
 const location = args[2];
 const upstreamsFile = args[3];
 const templatePath = args[4];
+const clientsFile = args[5] || "non-existent";
 
 const upstreamsFileContent = readFile(upstreamsFile, "[]");
+const clientsFileContent = readFile(clientsFile, '["all"]');
 const upstreams = JSON.parse(upstreamsFileContent);
+const allowedClients = JSON.parse(clientsFileContent);
+
 const servers = upstreams.map((upstream) => "server " + upstream + ";").join("\n    ");
+const mappedClients = allowedClients.map((client) => "allow " + client + ";");
+if (allowedClients != ["all"]) {
+    mappedClients.push("deny all;");
+}
+const clients = mappedClients.join("\n    ");
 
 const confTemplate = fs.readFileSync(templatePath, 'utf8');
 const confWithName = confTemplate.replace(/%NAME%/g, name);
 const confWithServerName = confWithName.replace(/%SERVER_NAME%/g, serverName);
 const confWithLocation = confWithServerName.replace(/%LOCATION%/g, location);
-const conf = confWithLocation.replace(/%SERVERS%/g, servers);
+const confWithClients = confWithLocation.replace(/%CLIENTS%/g, clients);
+const conf = confWithClients.replace(/%SERVERS%/g, servers);
 
 console.log(conf);
