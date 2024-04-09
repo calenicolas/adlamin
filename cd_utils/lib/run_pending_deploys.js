0a4d04c1af2983b10c8d19c0e936e3de45eb05dd
calenicolas <>
2024-04-09 13:44:31 -0300

    Adlamin CLI - V3.4

--- cd_utils/lib/run_pending_deploys.js
+++ cd_utils/lib/run_pending_deploys.js
@@ -5,24 +5,30 @@ const deploy = require("./deploy");
 const pendingDeploysDirectory = "/root/deploy-service/deploy/pending/";
 const doneDeploysDirectory = "/root/deploy-service/deploy/done/";
 
+function run(deploys) {
+    if (!deploys.length) return;
+
+    const pendingDeploy = deploys.pop()
+    const deployData = JSON.parse(pendingDeploy.content);
+    deploy(deployData, (error) => {
+        if (error) return console.error(error);
+        fs.renameSync(
+            pendingDeploysDirectory + pendingDeploy.fileName,
+            doneDeploysDirectory + pendingDeploy.fileName
+        );
+        run(deploys)
+    });
+}
+
 function runPendingDeploys() {
-    fs.readdirSync(pendingDeploysDirectory)
+    const deploys = fs.readdirSync(pendingDeploysDirectory)
         .map((fileName) => {
             return {
                 content: fs.readFileSync(pendingDeploysDirectory + fileName, { encoding: 'utf8' }),
                 fileName: fileName
             }
-        })
-        .forEach((pendingDeploy) => {
-            const deployData = JSON.parse(pendingDeploy.content);
-            deploy(deployData, (error) => {
-                if (error) return console.error(error);
-                fs.renameSync(
-                    pendingDeploysDirectory + pendingDeploy.fileName,
-                    doneDeploysDirectory + pendingDeploy.fileName
-                );
-            });
         });
+    run(deploys)
 }
 
 runPendingDeploys();
