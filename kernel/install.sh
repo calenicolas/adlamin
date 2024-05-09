a86fbfe333f51900294fb2e4943e6f262c3c9dac
calenicolas <>
2024-05-09 17:41:55 -0300

    Adlamin CLI - V3.4.1

--- kernel/install.sh
+++ kernel/install.sh
@@ -3,3 +3,6 @@
 mkdir -p /etc/adlamin/configuration/nginx
 cp configuration.json /etc/adlamin
 cp configuration/* /etc/adlamin/configuration
+
+echo "building adlamin-nginx image..."
+docker build -t adlamin-nginx ./docker/nginx
