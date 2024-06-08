b58d7a23fe8633e475aee33d8bfbcddab8a817e2
calenicolas <>
2024-06-08 19:30:57 -0300

    Adlamin CLI - V4.0.2

--- kernel/install.sh
+++ kernel/install.sh
@@ -5,6 +5,4 @@ cp configuration.json /etc/adlamin
 cp configuration/* /etc/adlamin/configuration
 
 echo "building adlamin-nginx image..."
-if ! docker -H $1 build -t adlamin-nginx ./docker/nginx ; then
-  exit 2
-fi
+docker -H $1 build -t adlamin-nginx ./docker/nginx
