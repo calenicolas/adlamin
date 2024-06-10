1d24a4323231dbda845c122f1f7e8eff0d364ea3
calenicolas <>
2024-06-10 16:20:30 -0300

    Adlamin CLI - V4.3.2

--- cd_utils/install.sh
+++ cd_utils/install.sh
@@ -24,12 +24,12 @@ cp -r service/systemctl/root/* /lib/systemd/system/
 systemctl daemon-reload
 systemctl enable adlamin-socket
 
-systemctl --user-unit=$1 daemon-reload
-systemctl --user-unit=$1 enable run-pending-deploys
-systemctl --user-unit=$1 enable run-pending-deploys.timer
-systemctl --user-unit=$1 enable load-docker-images
-systemctl --user-unit=$1 enable load-docker-images.timer
-systemctl --user-unit=$1 enable expire-clients
-systemctl --user-unit=$1 enable expire-clients.timer
-systemctl --user-unit=$1 enable adlamin-initialize
-systemctl --user-unit=$1 enable adlamin-cleanup
+systemctl -M $1@ --user daemon-reload
+systemctl -M $1@ --user enable run-pending-deploys
+systemctl -M $1@ --user enable run-pending-deploys.timer
+systemctl -M $1@ --user enable load-docker-images
+systemctl -M $1@ --user enable load-docker-images.timer
+systemctl -M $1@ --user enable expire-clients
+systemctl -M $1@ --user enable expire-clients.timer
+systemctl -M $1@ --user enable adlamin-initialize
+systemctl -M $1@ --user enable adlamin-cleanup
