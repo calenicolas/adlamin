fdef3fee73d3a56c05695d114426dad69563f38c
calenicolas <>
2024-06-10 11:41:35 -0300

    Adlamin CLI - V4.2.0

--- cd_utils/install.sh
+++ cd_utils/install.sh
@@ -23,6 +23,8 @@ cp -r service/systemctl/* /lib/systemd/system
 systemctl daemon-reload
 systemctl enable run-pending-deploys
 systemctl enable run-pending-deploys.timer
+systemctl enable load-docker-images
+systemctl enable load-docker-images.timer
 systemctl enable expire-clients
 systemctl enable expire-clients.timer
 systemctl enable adlamin-initialize
