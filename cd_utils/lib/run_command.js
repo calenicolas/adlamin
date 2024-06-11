77fcc23a3aae547e9376cb89d99a5148adb0f48a
calenicolas <>
2024-06-11 19:29:26 -0300

    Adlamin CLI - V4.7.0

--- cd_utils/lib/run_command.js
+++ cd_utils/lib/run_command.js
@@ -2,6 +2,21 @@ const { spawn } = require('child_process');
 
 module.exports = function(command, parameters, done) {
     const commandResult = spawn(command, parameters);
-    commandResult.on('close', () => { done() });
-    commandResult.on('error', (error) => { console.log(error); done() });
+    console.log("running ", command, parameters)
+    commandResult.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+      done();
+    });
+
+    commandResult.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+      done();
+    });
+
+    commandResult.on('close', (code) => {
+      console.log(`child process exited with code ${code}`);
+      done();
+    });
 }
+
+
