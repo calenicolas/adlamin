7dfb429543817c93ee9db709972700839f06f2b0
calenicolas <>
2024-06-28 18:01:00 -0300

    Adlamin CLI - V4.8.8

--- kernel/install.sh
+++ kernel/install.sh
@@ -4,5 +4,9 @@ mkdir -p /etc/adlamin/configuration/nginx
 cp configuration.json /etc/adlamin
 cp configuration/* /etc/adlamin/configuration
 
+mkdir -p /home/deploy/.config/systemd/user/docker.service.d
+cp ./override.conf /home/deploy/.config/systemd/user/docker.service.d
+chmod 644 /home/deploy/.config/systemd/user/docker.service.d/override.conf
+
 echo "building adlamin-nginx image..."
 docker -H $1 build -t adlamin-nginx ./docker/nginx
